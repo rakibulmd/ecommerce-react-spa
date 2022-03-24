a598f15fea09315a5623ff68d0a46d2e6e094a75
Rakibul md <rakib245875@gmail.com>
2022-03-24 19:54:11 +0600

    added react font awesome

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,3 +1,5 @@
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Products.css";
 
@@ -22,7 +24,8 @@ const Products = ({ product, addToCartHandler }) => {
                     addToCartHandler(product);
                 }}
             >
-                Add to Cart
+                Add to Cart{" "}
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
